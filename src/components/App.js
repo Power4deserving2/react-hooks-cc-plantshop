527de7787d71140dee8d68b1f87fa6af9df30d31
Immanuel <ipmaster40@gmail.com>
2025-05-03 21:03:36 +0300

    Added files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage
+        plants={filteredPlants}
+        onAddPlant={handleAddPlant}
+        search={search}
+        setSearch={setSearch}
+      />
     </div>
   );
 }
